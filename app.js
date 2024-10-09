40086af4496526a428bccd6245bbdad913dcdedc
unknown <syawqiarroyan@gmail.com>
2024-10-09 23:32:53 +0700

    Tambahin fungsi kontak ke dalam local

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const express = require("express");
 require("./mongo/db");
 const expressLayouts = require("express-ejs-layouts");
 const User = require("./mongodb/user");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const { body, validationResult, check } = require("express-validator");
 const Data = require("./mongodb/settings");
 const { get } = require("mongoose");
+const bcrypt = require("bcryptjs/dist/bcrypt");
 const app = express();
 const port = 3000;
 
@@ -19,12 +21,49 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// untuk halaman login
+// halaman registrasi akun
 
-router.post("/submitlogin", (req, res) => {
+app.post("/submitregister", async (req, res) => {
+  const { email, username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  // register user ke dalam database
+
+  const newUser = new User({
+    email,
+    username,
+    password: hashedPassword,
+  });
+  // data berhasil di masukkan ke dalam database
+  try {
+    newUser.save();
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// halaman login user dengan t
+app.post("/submitlogin", async (req, res) => {
   const { email, password } = req.body;
+  const findAuthUser = await User.findOne({ email });
 
-  // cek duplikat apakah sudah ada datanya di database
+  if (!findAuthUser) {
+    res.redirect("/");
+    return console.log("Username not found");
+  }
+  // ubah hashed dan bandingkan apakah sama
+  const validPassword = await bcrypt.compare(password, findAuthUser.password);
+  if (!validPassword) {
+    res.redirect("/");
+    return console.log("Wrong Password inserted");
+  }
+  const token = jwt.sign({ email: findAuthUser.email }, "secretKey", {
+    expiresIn: "1h",
+  });
+
+  res.setHeader("Authorization", token);
+
+  return res.redirect("/dashboard");
 });
 
 app.get("/", (req, res) => {
@@ -32,8 +71,12 @@ app.get("/", (req, res) => {
 });
 
 // setting untuk getnya
-app.get("/dashboard", (req, res) => {
+app.get("/dashboard", verifyUser, (req, res) => {
   res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 app.get("/tables", async (req, res) => {
@@ -71,6 +114,23 @@ app.post("/submit", (req, res) => {
 //     }
 //   }
 // );
+
+// buat function untuk validasi tiap token halaman
+
+function verifyUser(req, res, next) {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(403).send("Token not found");
+  }
+
+  jwt.verify(token, "secretKey", (err, decoded) => {
+    if (err) {
+      return res.status(403).send("Please Login first");
+    }
+    req.findAuthUser = decoded;
+    next();
+  });
+}
 
 app.get("/addform", (req, res) => {
   res.render("add");
